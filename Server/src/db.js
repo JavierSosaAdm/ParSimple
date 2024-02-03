d6f5ef2afb1e2fbf68043d2a62918b762f50e1f2
Javier.pjs <javier.sosa.1911@gmail.com>
2024-02-02 21:53:37 -0300

    nuevos modelos y relaciones categoria y cart

--- Server/src/db.js
+++ Server/src/db.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
 const userModel = require('./models/produtc');
 const productModel = require('./models/user');
-const product = require('./models/user');
+const cartModel = require('./models/cart');
+const categoryModel = require('./models/category');
 
 require('dotenv').config();
 
@@ -19,16 +20,31 @@ const dataBase = new Sequelize(
     { logging : false }
 )
 
-userModel(dataBase)
-productModel(dataBase)
-
+userModel(dataBase);
+productModel(dataBase);
+cartModel(dataBase);
+categoryModel(dataBase);
 // Relaciones
 
-const { Product, User } = dataBase.models;
+const { Product, User, Cart, Category } = dataBase.models;
 
-//*un usuario puede comprar varios productos y un producto puede ser comprado por varios usuarios
-User.belongsToMany(Product, {through: 'user_product'})
-Product.belongsToMany(User, {through: 'user_product'})
+//*un producto puede tener una categoria y una categoria puede tener varios productos 1:N
+Category.hasMany(Product, {foreignKey:"id_category"});
+Product.belongsTo(Category, {foreignKey:"id_category"});
+
+//*Un usuario puede comprar varios productos, y un producto puede ser comprado por varios usuarios N:N
+User.belongsToMany(Product, {through: 'user_product'});
+Product.belongsToMany(User, {through: 'user_product'});
+
+//*Un usuario puede tener una sola card, y una card puede ser de solo un usuario 1:1
+User.hasOne(Cart, { onDelete: 'CASCADE'});
+Cart.belongsTo(User);
+
+//*un usuario puede comprar varios productos y un producto puede ser comprado por varios usuarios N:N
+Cart.belongsToMany(Product, {through: 'cart_product', onDelete: 'CASCADE'});
+Product.belongsToMany(Cart, {through: 'cart_product', onDelete: 'CASCADE'});
+
+
 
 module.exports = {
     ...dataBase.models,
