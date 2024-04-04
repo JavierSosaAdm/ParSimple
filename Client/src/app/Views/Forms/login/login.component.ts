bc9183825e53ffc82e0530008bb7e49624202635
Javier.pjs <javier.sosa.1911@gmail.com>
2024-04-04 16:14:47 -0300

    nueva estructura con Angular y formulario login

--- Client/src/app/Views/Forms/login/login.component.ts
+++ Client/src/app/Views/Forms/login/login.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  loginForm!: FormGroup;
+
+  constructor(private FormBuilder: FormBuilder) {
+    this.loginForm = this.FormBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  enviar(event: Event){
+    event.preventDefault();
+    console.log(this.loginForm.value);
+  }
+  ngOnInit(): void {
+    console.log('esto es el login');
+  }
+
+  hasErrors(field: string, typeError: string) {
+    return this.loginForm.get(field)?.hasError(typeError) && this.loginForm.get(field)?.touched;
+  }
 }
