a60352d7aab9a4d720e1583f25ecae2ee00f0ade
Javier.pjs <javier.sosa.1911@gmail.com>
2024-06-14 16:26:28 -0300

    datos de cliente (primera parte)

--- Client/src/app/Views/client/client.component.ts
+++ Client/src/app/Views/client/client.component.ts
@@ -13,27 +13,28 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 export class ClientComponent implements OnInit {
   userList: { data: User, id: string }[] = [];
   filter: { data: User, id: string }[] = [];
+
   private _userService = inject(UserService);
   private _authService = inject(AuthService);
   private authService = inject(AngularFireAuth)
   public users: {data: User}[] = [];
   private currentEmail: string | null | undefined = '';
-  // private currentUser: 
-
-  ngOnInit(): void {
-    this.authService.authState.subscribe(async (user) => {
-      console.log('user: -->', user?.email); 
-      this._authService.getCurrentUser().subscribe(async (data) => {
-      
-        this.currentEmail = data?.email;
-        console.log('email: -->', this.currentEmail);
-        await this._userService.getUsers().subscribe((users) => {
-            this.userList = users
-            this.filter = this.userList.filter(user => user.data.email === this.currentEmail)
-            console.log('filter', this.filter);
-            return this.filter
-        })
+  
+  listaDeUsuarios() {
+    this._userService.getUsers().subscribe(async (users) => {
+      this.userList = users;
+      this._authService.getCurrentUser().subscribe((user) => {
+        this.currentEmail = user?.email
+        this.userList = this.userList.filter(user => user.data.email === this.currentEmail)
+        console.log('email dentro de usuarios: ', this.currentEmail);
       })
-    })   
+      console.log('esto es lista de user: ', this.userList);
+    })
+  };
+  ngOnInit(): void {
+    this.listaDeUsuarios();
   }
 }
+
+
+
